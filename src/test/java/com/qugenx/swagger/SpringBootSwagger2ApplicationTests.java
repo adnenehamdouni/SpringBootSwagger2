f123c6a7e70a98da7559cad21983fee9462b8ab4
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-11-29 18:55:44 +0100

    first commit

---
+++ src/test/java/com/qugenx/swagger/SpringBootSwagger2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qugenx.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwagger2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
